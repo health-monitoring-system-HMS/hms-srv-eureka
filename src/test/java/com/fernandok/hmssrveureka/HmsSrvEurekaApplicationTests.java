1bbd392967d6283017aca45ba4c7c93bd8ab5679
fernandokonopacki <luisfernandokonopacki@gmail.com>
2024-08-27 23:52:09 -0300

    [INIT]: Primeiro commit

---
+++ src/test/java/com/fernandok/hmssrveureka/HmsSrvEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandok.hmssrveureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsSrvEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
